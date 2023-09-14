ee34ddff1d47c1f5d4e3d480bdfb62fce6d50ddc
praveen chigurupati <praveen.chigurupati@actifio.com>
2023-09-14 15:45:09 -0500

    adding backend provider to store state file in storage container

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,3 +10,12 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstaterg"
+    storage_account_name = "intutivetfstatesa"
+    container_name       = "intutivetfstatesac"
+    key                  = "feature.terraform.tfstate"
+  }
+}
